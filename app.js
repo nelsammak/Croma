cacc8e71b8a1e87050d8c6ea525b97096fe24462
Mina <tigerdx619@gmail.com>
2015-03-12 00:04:35 +0200

    Changed the author for all books #1

--- app.js
+++ app.js
@@ -10,30 +10,30 @@
 	var books = [
 		{
 			name: "Harry Potter and the Philosopher's Stone",
-			author: "J. K. Rolling",
+			author: "J. K. Rowling",
 			image:  'Assets/Harry_Potter_and_the_Philosophers_Stone_Book_Cover.jpg'
 			
 		},
 		{
 			name: "Harry Potter and the Chamber of the Secrets",
-			author: "J. K. Rolling",
+			author: "J. K. Rowling",
 			image:  'Assets/Harry_Potter_and_the_Chamber_of_Secrets.jpg'
 		},
 		{
 			name: "Harry Potter and the Prisoner of Azkaban",
-			author: "J. K. Rolling",
+			author: "J. K. Rowling",
 			image:  'Assets/Harry_Potter_and_the_Prisoner_of_Azkaban.jpg'
 			
 		},
 		{
 			name: "Harry Potter and the Goblet of Fire",
-			author: "J. K. Rolling",
+			author: "J. K. Rowling",
 			image:  'Assets/Harry_Potter_and_the_Goblet_of_Fire.jpg'
 			
 		},
 		{
 			name: "Harry Potter and the Order of the Phoenix",
-			author: "J. K. Rolling",
+			author: "J. K. Rowling",
 			image:  'Assets/200px-Harry_Potter_and_the_Order_of_the_Phoenix.jpg'
 			
 		},
