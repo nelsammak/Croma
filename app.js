822070b06ad7723148cb30b0850e6b39b675692d
Mina <tigerdx619@gmail.com>
2015-03-11 14:54:29 +0200

    Added documentation #1

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 (function(){
 /*viewing books module*/
 	var app = angular.module('viewBooks', []);
+/*viewing books controller*/
 	app.controller('viewBooksController', function(){
 		this.product = book;
 	});	
-	var book = {
+		var book = {
 		name: "Harry Potter",
 		author: "J.K.Rolling"
 	};
