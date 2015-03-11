96ebeedf680c71fc46980359475029ca6108e844
Mina <tigerdx619@gmail.com>
2015-03-11 14:39:26 +0200

    added controller to app.js file #1

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 
+(function(){
 /*viewing books module*/
-var app = angular.module('viewBooks', []);
-
+	var app = angular.module('viewBooks', []);
+	app.controller('viewBooksController', function(){
+		this.product = book;
+	});	
+	var book = {
+		name: "Harry Potter",
+		author: "J.K.Rolling"
+	}
+})();
