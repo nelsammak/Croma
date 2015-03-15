0f4505791624991c63c0117dc370787368da46a4
nelsammak <nadah.125@gmail.com>
2015-03-15 16:13:37 +0200

    Deleted sqlite-related file from backend/config/ Edited config.json to remove sequelize and sqlite related references. Edited the db url for development phaze in config.json

--- backend/config/sqlite.js
+++
@@ -1,8 +0,0 @@
-module.exports.default = {
-    sqlite : function(api) {
-        return {
-            storage: __dirname + '/../store/app.sqlite',
-            dialect: 'sqlite'  
-        };
-    }
-};
