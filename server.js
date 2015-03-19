1ff5e0a0c214db6443aac584b6e96e09d2e8bb81
Mina <tigerdx619@gmail.com>
2015-03-19 14:17:06 +0200

    DB is working now :smile: #2

--- server.js
+++ server.js
@@ -4,31 +4,28 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 
 mongoose.connect('mongodb://localhost/test');
+
 var db = mongoose.connection;
 fs.readdirSync(__dirname+ '/models').forEach(function(filename){
 	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
 })
 
-  var Schema = mongoose.Schema;
-
-var booksSchema = new Schema({
+mongoose.model('books', {
 		name: String,
     	author: String,
     	genre :String,
     	description :String,
     	imagelocation:String
-});
-
-mongoose.Schema = ('books',booksSchema);
+})  ;
 
 app.get('/books', function (req, res){
 	console.log("I recieved a GET request");
 	mongoose.model('books').find(function (err, books){
-		res.json(docs);
+		res.send(books);
 		console.log("done");
 	});
 		
-	})
+	});
 
 
 
