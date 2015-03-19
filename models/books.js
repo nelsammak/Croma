43d1bf4e07ebbac2c223e3035b73a140cbfc3bb9
KarimElSheikh <kimo_xvirus@hotmail.com>
2015-03-19 18:36:05 +0200

    added 7 book inserts

--- models/books.js
+++ models/books.js
@@ -2,21 +2,76 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var booksSchema = new Schema({
-		name: String,
-    	author: String,  	
+	name: String,
+	author: String,
+	coverLocation: String
 });
 
 
-booksSchema.set('toJSON', { virtuals: true })
+booksSchema.set('toJSON', { virtuals: true });
 
 var books = mongoose.model('books',booksSchema);
 
-//insert books
+//inserting books
 var book1 = new books({ 
-	name: 'Karim',
-	author:'Mina' });
-	book1.save(function (err, book1) {
-  	if (err) return console.error(err);
+	name: 'Harry Potter and the Philosopher\'s Stone',
+	author:'J.K. Rowling',
+	coverLocation: 'assets/1.jpg'
+});
+book1.save(function (err, book1) {
+  if (err) return console.error(err);
 });
 
-	
+var book2 = new books({ 
+	name: 'Harry Potter and the Chamber of Secrets',
+	author:'J.K. Rowling',
+	coverLocation: 'assets/2.jpg'
+});
+book2.save(function (err, book2) {
+  if (err) return console.error(err);
+});
+
+var book3 = new books({ 
+	name: 'Harry Potter and the Prisoner of Azkaban',
+	author:'J.K. Rowling',
+	coverLocation: 'assets/3.jpg'
+});
+book3.save(function (err, book3) {
+  if (err) return console.error(err);
+});
+
+var book4 = new books({ 
+	name: 'Harry Potter and the Goblet of Fire',
+	author:'J.K. Rowling',
+	coverLocation: 'assets/4.jpg'
+});
+book4.save(function (err, book4) {
+  if (err) return console.error(err);
+});
+
+var book5 = new books({ 
+	name: 'Harry Potter and the Order of the Phoenix',
+	author:'J.K. Rowling',
+	coverLocation: 'assets/5.jpg'
+});
+book5.save(function (err, book5) {
+  if (err) return console.error(err);
+});
+
+var book6 = new books({ 
+	name: 'Harry Potter and the Half-Blood Prince',
+	author:'J.K. Rowling',
+	coverLocation: 'assets/6.jpg'
+});
+book6.save(function (err, book6) {
+  if (err) return console.error(err);
+});
+
+var book7 = new books({ 
+	name: 'Harry Potter and the Deathly Hallows',
+	author:'Joanne Kathleen Rowling',
+	coverLocation: 'assets/7.jpg'
+});
+book7.save(function (err, book7) {
+  if (err) return console.error(err);
+});
