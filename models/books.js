0d4678491b8bde705b1b58f5324f46b2a4f28140
Mina <tigerdx619@gmail.com>
2015-03-19 18:11:18 +0200

    modified books.js #2

--- models/books.js
+++ models/books.js
@@ -3,11 +3,20 @@ var Schema = mongoose.Schema;
 
 var booksSchema = new Schema({
 		name: String,
-    	author: String,
-    	
+    	author: String,  	
 });
 
 
 booksSchema.set('toJSON', { virtuals: true })
 
-mongoose.model('books',booksSchema);
+var books = mongoose.model('books',booksSchema);
+
+
+var book1 = new books({ 
+	name: 'Karim',
+	author:'Mina' });
+	book1.save(function (err, book1) {
+  	if (err) return console.error(err);
+});
+
+	
--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,7 +1,6 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
-$http.get('/books').success(function(response){
+    $http.get('/books').success(function(response){
 	console.log("I received the DATA")
 	 $scope.books=response;
 });
