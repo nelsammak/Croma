4929592e6e92eee607e6c6027cb6262ef62d06ce
Mina <tigerdx619@gmail.com>
2015-03-19 12:48:16 +0200

    Changing mongoose implementation #2

--- server.js
+++ server.js
@@ -4,36 +4,7 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  // yay!
-  var booksSchema = mongoose.Schema({
-    name: String,
-    author: String,
-    genre :String,
-    description :String,
-    imagelocation:String
-   })
 
-  var Book = mongoose.model('Book', booksSchema);
-  var NorthAndSouth = new Book({
-   name: 'North And South',
-   author: 'Elizabeth Gaskell',
-   genre: 'Classic Fiction & Romance',
-   description :'Elizabeth Gaskell skillfully fuses individual feeling with social concern, 
-   and in Margaret Hale creates one of the most 
-   original heroines of Victorian literature.',
-   imagelocation:'blabla'
-    })
-  console.log(Book.name);
-
-  NorthAndSouth.save(function (err, NorthAndSouth) {
-  	if (err) return console.error(err);
-  
-});
-Books.find({ name: /^NorthAndSouth/ }, callback)
-
-});
 
 
 
