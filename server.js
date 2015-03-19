75aac4cda296d55c451f91acfa6cbecb2ac7174e
Mina <tigerdx619@gmail.com>
2015-03-19 12:16:36 +0200

    testing connection to mongoose #2

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  // yay!
+});
 
 app.use(express.static(__dirname + '/public'));
 
