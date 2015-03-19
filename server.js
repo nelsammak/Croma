f32619777b705abe601428e8484877787a407afd
Mina <tigerdx619@gmail.com>
2015-03-19 12:15:00 +0200

    required mongoose and connected to it #2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
 app.use(express.static(__dirname + '/public'));
 
