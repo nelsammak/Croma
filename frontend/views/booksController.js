97bea792ff887a5198dd631d6cd592ee9e240c1d
KarimElSheikh <kimo_xvirus@hotmail.com>
2015-03-20 02:26:42 +0200

    Still needs Documentation

--- frontend/views/booksController.js
+++ frontend/views/booksController.js
@@ -1,7 +1,13 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-	$http.get('/books2').success(function(response) {
-		console.log("I received the DATA");
-	 	$scope.books2=response;
-	});
-}]);
+myApp.controller('AppCtrl', ['$scope', '$http',
+	function($scope, $http) {
+
+		$http.get('/books2').success(function(response) {
+			console.log("I received the DATA");
+			/*for(var i = 0; i < response.length; i++)
+				$scope["show" + response[i].id] = false;*/
+		 	$scope.books2=response;
+		});
+
+	}
+]);
