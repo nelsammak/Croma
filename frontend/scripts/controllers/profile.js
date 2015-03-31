c4014639282e77699e1deb291f2145bc58af43b4
Mohamed Yasser <mohamed.yasser.abdel.razek@gmail.com>
2015-03-31 23:33:25 +0200

    Fixed the get my profile bug el7amdullelah w rabena yostor #12

--- frontend/scripts/controllers/profile.js
+++ frontend/scripts/controllers/profile.js
@@ -2,7 +2,9 @@
 angular.module('angularPassportApp')
 .controller('ProfileController', ['$scope', '$http',
 	function sendUserInfo ($scope, $http) {
-		$http.get('api/users/550c63a6d4d5f5280d565015').success(function(response) {
+		// var id = $rootScope.currentUser._id;
+		$http.get('api/users/' + $scope.currentUser._id).success(function(response) {
+			console.log('scope current user', $scope.currentUser._id);
 			console.log(response);
 		 	$scope.userInfo=response;
 		});
