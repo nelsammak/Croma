8300b703dc36f0885de446f6d1cca26a4c57cee2
omarkamal95 <omarkamal95@gmail.com>
2015-04-06 19:23:45 +0200

    added button to test currently reading

--- frontend/scripts/controllers/books.js
+++ frontend/scripts/controllers/books.js
@@ -3,7 +3,6 @@
 angular.module('angularPassportApp')
 .controller('AppCtrl', ['$scope', '$http',
 	function sendBookCollection ($scope, $http) {
-
 		$http.get('/api/books').success(function(response) {
 		 	$scope.books=response;
 		 	
@@ -11,6 +10,11 @@ angular.module('angularPassportApp')
 			console.log("I received the DATA");
 		 	$scope.books=response;*/
 		});
+		$scope.sendReading = function(bookid){
+			$http.post('/api/books/'+ bookid +'/currentlyReading').success(function(response){
+				console.log(response);
+			});
+		}
 	}
 ]);
 //Book Controller's job is to send the book collection in JSON format
