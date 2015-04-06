699c4422442fbab49d9f69e6fc723aa7d6139c33
MinaNemr <tigerdx619@gmail.com>
2015-04-06 22:12:11 +0200

    added rate book function to book controller #33

--- frontend/scripts/controllers/book.js
+++ frontend/scripts/controllers/book.js
@@ -1,12 +1,20 @@
 'use strict';
 
-///Book Controller's job is to recieve a book and prepare it to be displayed with all its info
+///Book Controller's main job is to recieve a book and prepare it to be displayed with all its info
 angular.module('angularPassportApp')
   .controller('BookCtrl', function ($scope, $http, ShareService) {
    	$http.get('api/books/'+ShareService.getValue()).success(function(response) {
 			console.log("I received the book");
 		 	$scope.book=response.book;
 		});
+   	//rate function's job is to send the the backend the user who wants to rate a book and his rating
+   	$scope.rate = function(rating) {
+    $http
+    $scope.id = ShareService.getValue();
+    console.log($scope.id);
+   };
   });
+
+  
 
   
