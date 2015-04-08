f1794fb7ca6416ae1f9b4e34c1ea0aeecfbf4b6f
Mernawaseif <mernawaseif94@gmail.com>
2015-04-09 00:01:56 +0200

    Final Search

--- frontend/scripts/app.js
+++ frontend/scripts/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+
 angular.module('angularPassportApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngRoute',
   'http-auth-interceptor',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ui.router',
+  'elasticsearch'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
@@ -26,13 +29,27 @@ angular.module('angularPassportApp', [
         templateUrl: 'partials/books.html',
         controller: 'AppCtrl'
       })
+      .when('/profile', {
+        templateUrl: 'partials/userprofile.html',
+        controller: 'ProfileController'
+      })
+      .when('/thebook', {
+        templateUrl: 'partials/index.html',
+        controller: 'ReaderController'
+      })
       .otherwise({
         redirectTo: '/'
       });
+
+      
     $locationProvider.html5Mode(true);
   })
 
   .run(function ($rootScope, $location, Auth) {
+ 
+ $rootScope.contentsPath = '';
+    
+    $rootScope.metadata = {bookTitle: 'TDO'};
 
     //watching the value of the currentUser variable.
     $rootScope.$watch('currentUser', function(currentUser) {
@@ -49,3 +66,15 @@ angular.module('angularPassportApp', [
       return false;
     });
   });
+angular.module('elasticsearch',[]).factory('esFactory',['$http','$q',function($http,$q){
+var factory=config || {};
+config.connectionClass= AngularConnector;
+config.$http=$http;
+config.defer()=function();
+return $q.defer();
+};
+return new client (config);
+}; ]);
+
+
+}
