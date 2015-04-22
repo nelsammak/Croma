9aa6b45a030214de8455f2db4235920de99eaa43
Mohamed Yasser <mohamed.yasser.abdel.razek@gmail.com>
2015-04-22 17:31:31 +0200

    pagination in books done#68

--- frontend/scripts/controllers/searchResultCtrl.js
+++ frontend/scripts/controllers/searchResultCtrl.js
@@ -5,24 +5,35 @@ app.controller('searchResultCtrl', function($scope,$location, $interval, searchS
   
     $scope.books = [];
     $scope.users = [];
-  
-
-  $interval(function () {
-    $scope.users = searchService.getUsers();
-    $scope.books = searchService.getBooks();
     $scope.itemsPerPage = 12
     $scope.currentPage = 1;
-    $scope.totalItems = $scope.books.length;  
+    $scope.totalItems = 0;
+
+
+  $interval(function () {
+    if (searchService.getUsers() != $scope.users || $scope.books != searchService.getBooks()) {
+      $scope.users = searchService.getUsers();
+      $scope.books = searchService.getBooks();
+      $scope.totalItems = $scope.books.length;
+      $scope.filteredBooks = $scope.books.slice(0, $scope.itemsPerPage)
+    }
 
   }, 500);
-  
+
 
   $scope.pageCount = function () {
     return Math.ceil($scope.books.length / $scope.itemsPerPage);
   };
   
-  $scope.redirectToProfile = function redirectToProfile (user_id) {
-      $location.path('/profile');
+  $scope.pageChanged = function(page) {
+    $scope.currentPage = page;
   }
 
+  $scope.$watch('currentPage + itemsPerPage', function() {
+      var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+      var end = begin + $scope.itemsPerPage;
+
+      $scope.filteredBooks = $scope.books.slice(begin, end);
+    });
+  
 });
