55f20c93498b0f745d008cac85c1e466fac22f14
MinaNemr <tigerdx619@gmail.com>
2015-04-23 21:45:55 +0200

    findOneOrCreate plugin to avoid adding duplicates #71

--- backend/models/genres.js
+++ backend/models/genres.js
@@ -1,10 +1,14 @@
 var mongoose = require('mongoose');
+var findOneOrCreate = require('mongoose-find-one-or-create');
 var Schema = mongoose.Schema;
 
 //Creating the Genre Schema, the schema takes a JSON of the attributes of the Genres Schema
 var genresSchema = new Schema({
 	name: { type: String, required: true }
 });
+
+//findOneOrCreate plugin to avoid adding duplicates
+genresSchema.plugin(findOneOrCreate);
 
 //include Mongoose virtual fields in toJSON by default
 genresSchema.set('toJSON', { virtuals: true });
