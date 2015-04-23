de1592381332c3565eba49524ae63618c7d35e12
nelsammak <nadah.125@gmail.com>
2015-04-24 01:54:31 +0200

    added user and book to be referenced with the review book_review.js model is now completed

--- backend/models/book_review.js
+++ backend/models/book_review.js
@@ -1,9 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var ObjectId = Schema.types.ObjectId;
 
 var book_reviewSchema = new Schema({
-	
+	userId: { type: ObjectId, ref: 'user' },
+	bookId: { type: ObjectId, ref: 'book'},
 	body: { type: String, required: true },
 	upVotes: { type: Number},
 	downVotes: { type: Number}
-})
+});
+
+module.exports = mongoose.model('book_review', book_reviewSchema);
