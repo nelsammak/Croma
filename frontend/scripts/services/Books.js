22880d95f672cbc2a31a424621b3104c211b549a
MinaNemr <tigerdx619@gmail.com>
2015-04-27 00:06:39 +0200

    change service functions style #71

--- frontend/scripts/services/Books.js
+++ frontend/scripts/services/Books.js
@@ -3,47 +3,44 @@ angular.module('angularPassportApp')
  .service('ShareService', function(){
     var id = 0;
   
-    return {
-            /**
-           * @function getValue
-           * gets value of id of book
-           * @return {int} {id: 'id of a book'} 
-           */
-            getValue: function () {
-                return id;
-            },
-            /**
-            * @function setValue
-            * sets value of id of book
-            */
-            setValue: function(value) {
-                id = value;
-            }
-        };
- 
+  /**
+  * @function getValue
+  * gets value of id of book
+  * @return {int} {id: 'id of a book'} 
+  */
+  this.getValue = function getValue () {
+    return id;
+  }
+  /**
+  * @function setValue
+  * sets value of id of book
+  */
+  this.setValue = function setValue(value) {
+    id = value;
+  }
 });
 
  //a service to pass genre  from Genre Ctrl to Genre display Ctrl
 angular.module('angularPassportApp')
  .service('ShareService2', function(){
     var genre = '';
-    return {
-            /**
-           * @function getValue
-           * gets value of  genre name
-           * @return {String} {genre: 'genre name'} 
-           */
-            getValue: function () {
-                return genre;
-            },
-            /**
-            * @function setValue
-            * sets value of genre name
-            */
-            setValue: function(value) {
-                genre = value;
-            }
-        };
+
+  /**
+  * @function getValue
+  * gets value of  genre name
+  * @return {String} {genre: 'genre name'} 
+  */
+  this.getValue = function getValue () {
+    return genre;
+  }
+  /**
+  * @function setValue
+  * sets value of genre name
+  */
+  this.setValue = function setValue(value) {
+    genre = value;
+  }
+
  
 
 });
