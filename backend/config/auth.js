cc9576540f5d8f8881cc5d0f21ecd7e428a17bb2
Israa-Elwan <Israa.elwan94@gmail.com>
2015-04-30 12:43:54 +0300

    @81 ,addinh auth

---
+++ backend/config/auth.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8081/auth/google/callback'
+    }
+
+};
