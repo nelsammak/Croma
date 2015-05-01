4364da4d5b5bd0828be4c324b2a5b3743baa16db
MinaNemr <tigerdx619@gmail.com>
2015-05-01 21:48:29 +0200

    added remove from to be read function #106

--- frontend/scripts/controllers/myShelf.js
+++ frontend/scripts/controllers/myShelf.js
@@ -3,15 +3,29 @@
 angular.module('angularPassportApp')
 .controller('ShelfCtrl', function getShelves ($scope, $http){
 
-		$http.get('/api/users/' + $scope.currentUser._id + '/currentlyReading').success(function(response){
-			$scope.currentBooks = response;
-		});
-		$http.get('/api/users/' + $scope.currentUser._id + '/toBeReadBooks').success(function(response){
-			$scope.toBeRead = response;
-		});
-		$http.get('/api/users/' + $scope.currentUser._id + '/readBooks').success(function(response){
-			$scope.Read = response;
-		});
+	$http.get('/api/users/' + $scope.currentUser._id + '/currentlyReading').success(function(response){
+		$scope.currentBooks = response;
+	});
+	$http.get('/api/users/' + $scope.currentUser._id + '/toBeReadBooks').success(function(response){
+		$scope.toBeRead = response;
+	});
+	$http.get('/api/users/' + $scope.currentUser._id + '/readBooks').success(function(response){
+		$scope.Read = response;
+	});
 
-	}
-);
+	/**
+      * @function removeTobeRead
+      * @param {int} bookId - id of the book clicked on
+      * @deletes a book from user's to be read list
+    */
+	$scope.removeTobeRead = function(bookId) {
+      $http.delete('api/books/'+bookId+'/ToBeRead')
+        .success(function(response){
+        })
+        .error(function(data) {
+          console.log('Error: ' + data);
+        });
+    };
+
+	
+});
