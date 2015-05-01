2b311cae9111dd715f680389b0c6277316884d7e
KarimElSheikh <kimoxvirus@gmail.com>
2015-05-01 16:08:43 +0200

    Fixed the stuff Veegas mentioned

--- backend/views/bookclub.js
+++ backend/views/bookclub.js
@@ -1,6 +1,7 @@
 'use strict';
 var User = require('../models/user.js'),
   BookClubs = require('../models/bookclub.js');
+var mongoose = require('mongoose');
 
 module.exports = function(router) {
   router.route('/bookclubs/createbookclub').post(function createBookClub(req, res, next) {
@@ -9,16 +10,18 @@ module.exports = function(router) {
         res.status(404).json(err);
         return next(err);
       }
-      var bookClub = new BookClubs({creator: req.body.userId, name: req.body.title, users: [req.body.userId]});
+      var bookClub = new BookClubs({creator: mongoose.Types.ObjectId(req.body.userId),
+        name: req.body.title,
+        users: [mongoose.Types.ObjectId(req.body.userId)]});
       bookClub.save();
       console.log("hi");
       var config = require('../config/config.json')[process.env.NODE_ENV];
       console.log(config.db.url);
       console.log(req.body.userId);
-      user.bookClubs.push(bookClub._id);
+      user.bookClubs.push(mongoose.Types.ObjectId(bookClub._id));
       user.markModified('bookClubs');
       user.save();
-      res.status(200).json("Created the Book Club Successfully");
+      res.status(201).json("Created the Book Club Successfully");
     });
   });
 
@@ -28,11 +31,11 @@ module.exports = function(router) {
         res.status(404).json(err);
         return next(err);
       }
-      BookClubs.find({users: req.body.userId}, function (err, bookClubsResult) {
+      BookClubs.find({users: mongoose.Types.ObjectId(req.body.userId)}, function (err, bookClubsResult) {
         if (err) {
           return next(err);
         }
-        res.status(200).json(bookClubsResult);
+        res.status(201).json(bookClubsResult);
       });
     });
   });
