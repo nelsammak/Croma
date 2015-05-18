3fbd3fb5b7fe878b0322477c1d6af180782bb2d3
veegas <mohamed.yasser.abdel.razek@gmail.com>
2015-05-18 17:25:21 +0200

    Sala7t el zorar beta3 el menu lama teb2a toggled

--- frontend/scripts/services/Auth.js
+++ frontend/scripts/services/Auth.js
@@ -4,7 +4,6 @@ angular.module('croma')
   .factory('Auth', function Auth($location, $rootScope, Session, User, $cookieStore) {
     $rootScope.currentUser = $cookieStore.get('user') || null;
     $cookieStore.remove('user');
-    console.log($rootScope)
     return {
 
       login: function(provider, user, callback) {
